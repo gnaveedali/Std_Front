449de98b4d0b678f0fc868893a257c4d39b202a4
naveed.gulam <naveed.gulam@systemsltd.com>
2022-08-08 16:42:32 +0500

    test

--- src/app/sales/sale-invoice/sale-invoice.component.ts
+++ src/app/sales/sale-invoice/sale-invoice.component.ts
@@ -19,7 +19,7 @@ import { VoucherPeriodSelectionService } from 'src/app/Services/voucher-period-s
   styleUrls: ['./sale-invoice.component.scss']
 })
 
-
+//ftc
 export class SaleInvoiceComponent implements OnInit {
 
   @ViewChild('openPeriodSelectionModal') openPeriodSelectionModal!: TemplateRef<any>;
